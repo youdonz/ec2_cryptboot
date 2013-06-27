096fee533fce9d1db890abacd9a7ff887b40f974
Tim Goddard <tim.goddard@youdo.co.nz>
2013-06-27 12:37:50 +1200

    Add in cloudinit config, compressed boot script

--- cryptboot.rb
+++ cryptboot.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'yaml'
+
 directory = ARGV.shift || "example"
 uri_path = ARGV.shift || "http://localhost"
 command = ARGV.shift || "#{directory}/init.sh"
@@ -18,13 +20,25 @@ system cmd
 
 File.unlink("#{boot_id}.key")
 
-boundary = `openssl rand -hex 20`.chomp
+config_file = "#{directory}/config.yml"
+config = {}
+if File.exists?(config_file)
+  config = YAML.load_file(config_file)
+end
 
+# Ensure that wget and openssl are present
+config["packages"] ||= []
+config["packages"] += ['wget', 'openssl']
+
+boundary = `openssl rand -hex 20`.chomp
 bootscript = <<-END
 MIME-Version: 1.0
 Content-Type: multipart/mixed; boundary=#{boundary}
 
-This is a message with multiple parts in MIME format.
+--#{boundary}
+Content-Type: text/cloud-config
+
+#{YAML.dump(config)}
 --#{boundary}
 Content-Type: text/x-shellscript
 
@@ -35,12 +49,12 @@ tar zxf '#{boot_id}.tgz'
 --#{boundary}--
 END
 
-File.open("out/#{boot_id}.boot", "w") do |bootfile|
+IO.popen("gzip -c > out/#{boot_id}.boot.gz", "w") do |bootfile|
   bootfile << bootscript
 end
 
 STDERR.puts "---"
 STDERR.puts "Upload out/#{boot_id}.tgz.enc to #{uri_path}/#{boot_id}.tgz.enc"
-STDERR.puts "Start Ubuntu EC2 VMs with out/#{boot_id}.boot as boot data"
+STDERR.puts "Start Ubuntu EC2 VMs with out/#{boot_id}.boot.gz as boot data"
 STDERR.puts "VMs will unpack archive and run #{command}"
 STDERR.puts "---"
