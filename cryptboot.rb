a9c5edb877fc0ebde7572d9287c9a3521cc52758
Tim Goddard <tim.goddard@youdo.co.nz>
2013-06-27 12:26:03 +1200

    Initial version

---
+++ cryptboot.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+
+directory = ARGV.shift || "example"
+uri_path = ARGV.shift || "http://localhost"
+command = ARGV.shift || "#{directory}/init.sh"
+
+system "mkdir out"
+
+boot_id = `openssl rand -hex 4`.chomp
+key = `openssl rand -hex 20`.chomp
+
+File.open("#{boot_id}.key", 'w') do |keyfile|
+  keyfile << key
+end
+
+cmd = "tar zc #{directory} | openssl enc -pass 'file:#{boot_id}.key' -out 'out/#{boot_id}.tgz.enc'"
+system cmd
+
+File.unlink("#{boot_id}.key")
+
+boundary = `openssl rand -hex 20`.chomp
+
+bootscript = <<-END
+MIME-Version: 1.0
+Content-Type: multipart/mixed; boundary=#{boundary}
+
+This is a message with multiple parts in MIME format.
+--#{boundary}
+Content-Type: text/x-shellscript
+
+wget -o '#{boot_id}.tgz.enc' '#{uri_path}/#{boot_id}.tgz.enc'
+openssl enc -d -pass 'pass:#{key}' -in '#{boot_id}.tgz.enc' -out '#{boot_id}.tgz'
+tar zxf '#{boot_id}.tgz'
+#{command}
+--#{boundary}--
+END
+
+File.open("out/#{boot_id}.boot", "w") do |bootfile|
+  bootfile << bootscript
+end
+
+STDERR.puts "---"
+STDERR.puts "Upload out/#{boot_id}.tgz.enc to #{uri_path}/#{boot_id}.tgz.enc"
+STDERR.puts "Start Ubuntu EC2 VMs with out/#{boot_id}.boot as boot data"
+STDERR.puts "VMs will unpack archive and run #{command}"
+STDERR.puts "---"
