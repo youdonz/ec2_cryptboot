2b6cd71dff88908ba8062f3162713be2184d4888
Tim Goddard <tim.goddard@youdo.co.nz>
2013-11-12 12:31:44 +1300

    Submodule support

--- cryptboot.rb
+++ cryptboot.rb
@@ -1,5 +1,51 @@
 #!/usr/bin/env ruby
 
+def config_merge(a, b)
+  if a.nil? || b.nil?
+    a || b
+  elsif a.kind_of?(Array) || b.kind_of?(Array)
+    a = [a] unless a.kind_of?(Array)
+    b = [b] unless b.kind_of?(Array)
+    (a + b).uniq
+  elsif a.kind_of?(Hash) && b.kind_of?(Hash)
+    a = a.dup
+    b.keys.each do |key|
+      if a.has_key?(key)
+        a[key] = config_merge(a[key], b[key])
+      else
+        a[key] = b[key]
+      end
+    end
+    a
+  else
+    b
+  end
+end
+
+def load_config(directory)
+  config = {}
+  config_file = "#{directory}/config.yml"
+  if File.exists?(config_file)
+    config = YAML.load_file(config_file)
+  end
+  
+  if submodules = config.delete('submodules')
+    sc = nil
+    submodules.each do |submodule|
+      sub_config = load_config("#{directory}/#{submodule}")
+      if sc.nil?
+        sc = sub_config
+      else
+        sc = config_merge(sc, sub_config)
+      end
+    end
+    
+    config = config_merge(sc, config)
+  end
+  
+  config
+end
+
 require 'yaml'
 
 directory = ARGV.shift || "example"
@@ -15,13 +61,8 @@ key = `openssl rand -hex 20`.chomp
 cmd = "tar zch #{directory} | openssl enc -aes-256-cbc -pass 'pass:#{key}' -out 'out/#{boot_id}.tgz.enc'"
 system cmd
 
-config_file = "#{directory}/config.yml"
+config = load_config(directory)
 command = "./init.sh"
-
-config = {}
-if File.exists?(config_file)
-  config = YAML.load_file(config_file)
-end
 
 # Ensure that wget and openssl are present
 config["packages"] ||= []
